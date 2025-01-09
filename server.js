5cacafe631bb496a0a562d9d6d423b8ea5512800
MiriamCall <bow18005@byui.edu>
2025-01-09 12:55:27 -0700

    adding lesson 1 code assignment

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello hubby!!!");
+});
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log("web server is listening at port " + port);
+console.log(`http://localhost:${port}/`);
